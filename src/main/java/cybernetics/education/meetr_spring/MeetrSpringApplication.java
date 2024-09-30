5219d638ce74adf2b561018d6531b656c6a44962
Kristex95 <kristex95@gmail.com>
2024-09-30 14:24:21 +0300

    Initial commit for spring app

---
+++ src/main/java/cybernetics/education/meetr_spring/MeetrSpringApplication.java
@@ -0,0 +1,13 @@
+package cybernetics.education.meetr_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetrSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetrSpringApplication.class, args);
+	}
+
+}
